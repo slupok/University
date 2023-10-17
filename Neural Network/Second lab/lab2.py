6786e13bc52f684b4b1588ab698a0b8211c66499
Maksim <slupoke@yandex.ru>
2023-10-18 00:58:06 +0300

    - немного поигрался с keras

--- Neural Network/Second lab/lab2.py
+++ Neural Network/Second lab/lab2.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
@@ -11,7 +12,6 @@ from tensorflow.keras.layers import Dense
 os.chdir('c:/University/Neural Network/Second lab')
 data = X = pd.read_csv("healthcare-dataset-stroke-data.csv")
 
-#Кодируем нечисловые данные
 data['gender'] = pd.factorize(data['gender'])[0]
 data['ever_married'] = pd.factorize(data['ever_married'])[0]
 data['work_type'] = pd.factorize(data['work_type'])[0]
@@ -29,15 +29,26 @@ X = data.drop(['stroke'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=21)
 
 model = Sequential()
-model.add(Dense(25, input_dim=10, activation="linear"))
+model.add(Dense(15, input_dim=10, activation="linear"))
 model.add(Dense(25, activation="relu"))
 model.add(Dense(20, activation="linear"))
 model.add(Dense(20, activation="relu"))
 model.add(Dense(1, activation="linear"))
 model.add(Dense(1, activation="sigmoid"))
 
+#Configures the model for training.
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+#Trains the model for a fixed number of epochs (dataset iterations).
 history = model.fit(X_train, y_train, epochs=15, validation_data=(X_test, y_test))
 
-print(f"Точность: {model.evaluate(X_test, y_test)[1]}")
-print(f"Предсказание: {model.predict(X_test).round().T[0]}")
+model.summary()
+
+print("first weights:", model.weights[0])
+print(f"test predict: {model(tf.ones((1, 10))).numpy()[0][0].round()}")
+
+#Returns the loss value & metrics values for the model in test mode.
+print(f"Eval: {model.evaluate(X_test, y_test)[1]}")
+
+#Generates output predictions for the input samples.
+print(f"Predict: {model.predict(X_test).round().T[0]}")
